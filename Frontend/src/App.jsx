a89f947031ed51f07a36a17ac9f27a1b47609a62
sushant-wayal <sushantssr12345@gmail.com>
2024-02-17 14:49:03 +0530

    cleaned Context

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,8 @@
 import { Outlet } from "react-router-dom"
-import { UserProvider } from "./Context/User"
-import { useState } from "react"
 
 const App = () => {
-	const [jwt, setJwt] = useState("");
 	return (
-		<UserProvider value={{ jwt, setJwt }}>
-			<Outlet/>
-		</UserProvider>
+		<Outlet/>
 	)
 }
 
